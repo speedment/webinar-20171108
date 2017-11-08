84129427f5b4497fb839001486737faaeae33d40
pyknic <emil@speedment.com>
2017-11-08 19:36:11 +0100

    Add more complex stream

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import com.speedment.example.webinar.db.WebinarApplication;
 import com.speedment.example.webinar.db.WebinarApplicationBuilder;
+import com.speedment.example.webinar.db.sakila.sakila.film.Film;
 import com.speedment.example.webinar.db.sakila.sakila.film.FilmManager;
+import com.speedment.runtime.core.ApplicationBuilder;
 
 /**
  * @author Emil Forslund
@@ -9,11 +11,14 @@ import com.speedment.example.webinar.db.sakila.sakila.film.FilmManager;
 public final class Main {
     public static void main(String... args) {
         WebinarApplication app = new WebinarApplicationBuilder()
+            .withLogging(ApplicationBuilder.LogType.STREAM)
             .withPassword("sakila")
             .build();
 
         FilmManager films = app.getOrThrow(FilmManager.class);
-        System.out.println("Film #: " + films.stream().count());
+        System.out.println("Film #: " + films.stream()
+            .filter(Film.LENGTH.greaterThan(40))
+            .count());
 
         app.stop();
     }
